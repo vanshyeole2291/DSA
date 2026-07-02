51f0dcb7d5a8c71fb776fdad85417abe2360f104
vansh yeole <106605513+vanshyeole2291@users.noreply.github.com>
2026-07-02 21:41:30 +0530

    Time: 0 ms (100%), Space: 8.6 MB (38.11%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0){
-            return false;
-        }
         int temp=x;
         long r=0;
-        while(x!=0){
-            int d = x%10;
+        while(x>0){
+            int d=x%10;
             r = r*10+d;
             x/=10;
         }
-        return temp==r;
+        return r==temp;
     }
 };
