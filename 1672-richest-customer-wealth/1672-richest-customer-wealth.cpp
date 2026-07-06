e940ab14fe0b3c48989ed92382e106c21fdfc301
vansh yeole <106605513+vanshyeole2291@users.noreply.github.com>
2026-07-06 16:23:19 +0530

    Time: 0 ms (100%), Space: 11.4 MB (20.83%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
+        int n=accounts.size();
         int ans=0,m=0;
-        for(int i=0;i<accounts.size();i++){
-                    
-                    ans=0;
+        for(int i=0;i<n;i++){
+            ans=0;
             for(int j=0;j<accounts[i].size();j++){
-               
-                ans+=accounts[i][j];  
-            }
-            m=max(m,ans);
+                ans+=accounts[i][j];
         }
-        return m;
+        m=max(m,ans);
+    }
+    return m;
     }
 };
