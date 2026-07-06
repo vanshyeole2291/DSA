7c8aa69dba2a11ab8384d8e2bc9e1e8d54c11901
vansh yeole <106605513+vanshyeole2291@users.noreply.github.com>
2026-07-06 15:29:48 +0530

    Time: 1 ms (19.14%), Space: 16.7 MB (95.45%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cpp
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
-        int n =nums.size();
-        vector<int> ans(n*2);
-       for(int i=0;i<n*2;i++){
-            ans[i]=nums[i%n];
-        }
-        
-        return ans;
+      int n=nums.size();
+      vector<int> temp(n*2);
+      for(int i=0;i<n*2;i++){
+        temp[i]=nums[i%n];
+      }
+      return temp;
     }
 };
