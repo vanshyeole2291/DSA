4906cd55374b51ac4506fdd2db847a9cf6a3af8c
vansh yeole <106605513+vanshyeole2291@users.noreply.github.com>
2026-07-06 16:34:44 +0530

    Time: 0 ms (100%), Space: 11.4 MB (56.72%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -4,11 +4,12 @@ public:
         int n=accounts.size();
         int ans=0,m=0;
         for(int i=0;i<n;i++){
-            ans=0;
+            
             for(int j=0;j<accounts[i].size();j++){
                 ans+=accounts[i][j];
         }
         m=max(m,ans);
+        ans=0;
     }
     return m;
     }
