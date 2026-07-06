758c9a21606f2d8697b72e191b42511f1a1e16ea
vansh yeole <106605513+vanshyeole2291@users.noreply.github.com>
2026-07-06 16:43:24 +0530

    Time: 0 ms (100%), Space: 9.8 MB (51.86%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        int n =nums.size();
-        int ans=0;
+        int ans=0,n=nums.size();
         for(int i=0;i<n;i++){
-           for(int j=i+1;j<n;j++){
-            if(nums[i]==nums[j]){
-                ans++;
+            for(int j=i+1;j<n;j++){
+                if(nums[i]==nums[j]){
+                    ans++;
+                }
             }
-           }
-            }
-       
+        }
         return ans;
     }
 };
