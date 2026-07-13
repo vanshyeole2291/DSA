8cbee4c40b5fc5197f7b2a2dea7c4d466e451beb
vansh yeole <106605513+vanshyeole2291@users.noreply.github.com>
2026-07-13 16:46:25 +0530

    Time: 0 ms (100%), Space: 13.5 MB (14.53%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int mostWordsFound(vector<string>& sentences) {
-        int c=0,m=0;
-
-        for(int i=0;i<sentences.size();i++){
-            c=1;
-            string s =sentences[i];
-            for(char ch : s){
-                if(ch==' '){
-                    c++;
-                }
+       int c=0,m=0;
+       for(int i=0;i<sentences.size();i++){
+        c=1;
+        string s= sentences[i];
+        for(char ch : s){
+            if(ch==' '){
+                c++;
             }
-            m=max(m,c);
         }
-        return m;
+        m=max(m,c);
+       }
+       return m;
     }
 };
