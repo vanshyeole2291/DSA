1300495fca1911cd708e27941e5b481f839468b6
vansh yeole <106605513+vanshyeole2291@users.noreply.github.com>
2026-07-14 14:15:50 +0530

    Time: 75 ms (22.09%), Space: 111.3 MB (13.89%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> st;
+        for(int i:nums){
+            if(st.find(i)==st.end()){
+                st.insert(i);
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+};
