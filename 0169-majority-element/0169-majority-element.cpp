a4bd6cf178de5a258cb2983fa119b5d073d3a1ba
vansh yeole <106605513+vanshyeole2291@users.noreply.github.com>
2026-07-21 18:32:03 +0530

    Time: 3 ms (27.96%), Space: 28.4 MB (11.45%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        return nums[nums.size()/2];
+        unordered_map<int,int> m;
+        int mf=0,ans=0;
+         for(int i : nums){
+            m[i]++;
+         }
+
+         for(auto p:m){
+            if(p.second>mf){
+                mf=p.second;
+                ans=p.first;
+            }
+         }
+         return ans;
     }
 };
