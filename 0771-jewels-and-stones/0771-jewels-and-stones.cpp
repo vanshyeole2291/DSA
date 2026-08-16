13fd15bae853cb56107883082d04a6768a58ced5
vansh yeole <106605513+vanshyeole2291@users.noreply.github.com>
2026-08-16 14:29:14 +0530

    Time: 0 ms (100%), Space: 8.5 MB (54.52%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.cpp
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-        unordered_set<char> s(jewels.begin(),jewels.end());
+        unordered_set<int>  s(jewels.begin(),jewels.end());
         int ans=0;
+
         for(int i=0;i<stones.size();i++){
-            if(s.count(stones[i]))
+            if(s.count(stones[i])){
                 ans++;
+            }
         }
         return ans;
     }
