77f98108b15855b5ecc1cc1f2093291af30dc8d2
vansh yeole <106605513+vanshyeole2291@users.noreply.github.com>
2026-08-19 10:48:46 +0530

    Time: 3 ms (66.5%), Space: 15 MB (12.18%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,7 +4,7 @@ public:
       unordered_map<int,int> m;
       for(int i=0;i<nums.size();i++){
         if(m.find(target-nums[i])!=m.end()){
-            return{m[target-nums[i]],i};
+            return {m[target-nums[i]],i};
         }
         m[nums[i]]=i;
       }
