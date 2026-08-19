7277b464236b7b785b8fcc3f1e9953205d105067
vansh yeole <106605513+vanshyeole2291@users.noreply.github.com>
2026-08-19 12:10:13 +0530

    Time: 0 ms (100%), Space: 9.5 MB (30.82%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -9,20 +9,21 @@ public:
 
         for(int i=0;i<s.size();i++){
             if(m1.find(s[i])!=m1.end()){
-            if(m1[s[i]]!=t[i]){
-                return false;
-            }
+                if(m1[s[i]]!=t[i]){
+                    return false;
+                }
             }else{
                 m1[s[i]]=t[i];
             }
             if(m2.find(t[i])!=m2.end()){
-            if(m2[t[i]]!=s[i]){
-                return false;
-            }  
-        }else{
+                if(m2[t[i]]!=s[i]){
+                    return false;
+                }
+            }else{
                 m2[t[i]]=s[i];
             }
-     }
-       return true;
+            
+        }
+        return true;
     }
 };
