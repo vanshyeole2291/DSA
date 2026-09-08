ae4e3603406fa2f05f30a60de8ea8beff1490a47
vansh yeole <106605513+vanshyeole2291@users.noreply.github.com>
2026-09-08 10:44:20 +0530

    Time: 8 ms (37.47%), Space: 134.3 MB (64.55%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
-        int n = nums.size();
-        int i =0,j=0;
-        long long sum=0;
-        vector<int> avg(n,-1);
+       int n  = nums.size();
+       vector<int> avg(n,-1);
+       int i=0,j=0;
+       long long sum =0;
 
-        while(j<n){
-            sum+=nums[j];
-            if(j-i+1 < 2*k+1){
-                j++;
-            }else if(j-i+1== 2*k+1){
-                avg[i+k] = sum / (2*k+1);
-                sum-=nums[i];
-                i++;j++;
-            }
+       while(j<n){
+        sum+=nums[j];
+        if(j-i+1 < 2*k+1){
+            j++;
+        }else if(j-i+1 == 2*k+1){
+            avg[i+k]= sum / (2*k+1);
+            sum-=nums[i];
+            i++;j++;
         }
-        return avg;
+       }
+       return avg;
     }
 };
