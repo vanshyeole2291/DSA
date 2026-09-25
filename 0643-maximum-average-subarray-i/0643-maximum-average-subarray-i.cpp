ab297ecb09fecf5f8ead6498aeb2d91117bc2aff
vansh yeole <106605513+vanshyeole2291@users.noreply.github.com>
2026-09-25 18:54:22 +0530

    Time: 4 ms (25.09%), Space: 113.7 MB (86.47%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -4,18 +4,15 @@ public:
         int i=0,j=0;
         int n = nums.size();
         int ans=INT_MIN,sum=0;
-
         while(j<n){
             sum+=nums[j];
-            if(j-i+1<k){
-                j++;
-            }else if(j-i+1==k){
-                ans=max(ans,sum);
-                sum-=nums[i];
-                i++;
-                j++;
-            }
+         if(j-i+1==k){
+            ans=max(ans,sum);
+            sum-=nums[i];
+            i++;
         }
-        return (double)ans/k;
+        j++;
+        }
+       return (double)ans/k;
     }
 };
